feef32b132225fafb424bbfd35dbfac49f31d452
bodhibrdr <julie.gumerman@gmail.com>
2020-05-04 17:17:57 -0600

    outside slideshow is there but not working yet

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,25 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+
+let outsideSlideIndex = 1;
+showsSlidesOutside(outsideSlideIndex)
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+const plusSlidesOutside = (b) => {
+  showsSlidesOutside(slideIndex += b)
+}
+
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
+}
+
+function currentSlideOutside(b) {
+  showsSlidesOutside(outsideSlideIndex = b)
 }
 
 function showSlides(n) {
@@ -20,5 +31,15 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+}
+
+const showSlidesOutside = (b) => {
+  let j;
+  let outsideSlides = document.getElementsByClassName("mySlides-o")
+  if (b > outsideSlides.length) {outsideSlideIndexlideIndex = 1}
+  if (b < 1) { outsideSlideIndex = outsideSlides.length}
+  for (j = 0; j < slides.length; j++) {
+    outsideSlides[i].style.display = "none";
+}
+  outsideSlides[outsideSlideIndex-1].style.display = "block";
 }
